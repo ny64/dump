7d6001494280bea9e0f9f6d7ba4ed4993066c212
Peter <peter@debian-BULLSEYE-live-builder-AMD64>
2022-08-13 14:11:21 +0200

    Fixes

--- worst_setup_ever.sh
+++ worst_setup_ever.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
-apps="zsh terminator git qbittorrent emacs vim picom herbstluftwm"
+salt=$RANDOM
+
+apps="zsh terminator git qbittorrent emacs vim picom herbstluftwm rofi"
 
 sudo apt update
 sudo apt install $apps 
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-mkdir -p ~/.tmp && cd ~/.tmp
+mkdir -p ~/.tmp_$salt && cd ~/.tmp_$salt
 git clone https://github.com/ny64/vim-setup
-cp vim-setup/.* ~/
+cp -R ./vim-setup/.* ~/
 
-git clone https://github.com/ny64/dump
-mkdir -p ~/.config/herbstluftwm && cp dump/herbstluftwm/* ~/.config/herbstluftwm
-mkdir -p "~/.config/gtk-3.0" && cp "dump/gtk-3.0/*" "~/.config/gtk-3.0"
-cp picom/picom.conf ~/.config/
-cp emacs/.* ~/
-cp -R terminator ~/.config/
-sudo mkdir -p /etc/X11/xorg.conf.d && sudo cp X11/* /etc/X11/xorg.conf.d
-cp zsh/.zshrc ~/
+git clone https://github.com/ny64/dump && cd dump
+mkdir -p ~/.config/herbstluftwm && cp ./herbstluftwm/* ~/.config/herbstluftwm
+mkdir -p ~/.config/gtk-3.0 && cp ./gtk-3.0/* ~/.config/gtk-3.0
+cp ./picom/picom.conf ~/.config/
+cp -R ./emacs/.* ~/
+cp -R ./terminator ~/.config/
+sudo mkdir -p /etc/X11/xorg.conf.d && sudo cp ./X11/* /etc/X11/xorg.conf.d
+cp ./zsh/.zshrc ~/
+
+rm -rf ~/.tmp_$salt
 
